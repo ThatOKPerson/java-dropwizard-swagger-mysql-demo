98ed69dca03e37e4a0b83f462238dfc525193fcc
ThatOKPerson <oran.kelly@kainos.com>
2023-08-25 13:52:16 +0100

    Updating product name to allow 100 characters

--- src/main/java/org/kainos/ea/core/ProductValidator.java
+++ src/main/java/org/kainos/ea/core/ProductValidator.java
@@ -4,8 +4,8 @@ import org.kainos.ea.cli.ProductRequest;
 
 public class ProductValidator {
     public static String isValidProduct(ProductRequest product){
-        if (product.getName().length() > 75){
-            return "name greater than 75 characters";
+        if (product.getName().length() > 100){
+            return "name greater than 100 characters";
         }
 
         if (product.getDescription().length() > 500){
